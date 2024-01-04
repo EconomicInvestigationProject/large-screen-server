36c013b0a5956b12424123a07e649666311d96ae
reginyuan <2496943860@qq.com>
2024-01-04 11:20:13 +0800

    feat: 修改代码

--- config/index.js
+++ config/index.js
@@ -2,13 +2,29 @@
  * 配置文件
  */
 
-// 配置 ClickHouse 数据库连接 数据库配置
+// 配置公司 ClickHouse 数据库连接 数据库配置
+// const clickhouseDbConfig = {
+//   host: "http://218.56.104.54:8123",
+//   user: "default",
+//   password: "Welcome1",
+//   database: "facedev"
+// };
+
+// 配置公安服务器 ClickHouse 数据库连接 数据库配置
+// const clickhouseDbConfig = {
+//   host: "http://192.168.6.111:8123",
+//   user: "default",
+//   password: "Welcome1",
+//   database: "facedev"
+// };
+
 const clickhouseDbConfig = {
-  host: "http://218.56.104.54:8123",
+  host: "http://127.0.0.1:8123",
   user: "default",
   password: "Welcome1",
   database: "facedev"
 };
+
 module.exports = {
   URL,
   clickhouseDbConfig
