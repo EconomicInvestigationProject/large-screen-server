a1312d49b74b3da722705197397a8d474852f36a
reginyuan <2496943860@qq.com>
2024-01-10 16:30:36 +0800

    feat: 新增重点人员功能

--- routes/keypersonnel.js
+++ routes/keypersonnel.js
@@ -25,6 +25,40 @@ router.get("/statistics", async (ctx) => {
 });
 
 /**
+ * 小区新增重点人员
+ */
+router.post("/addRecord", async (ctx) => {
+  const { idCard, name, status } = ctx.request.body;
+  let condition = ""; //状态
+  // 获取今天的日期
+  let today = new Date();
+  // 格式化为 YYYY-MM-DD
+  let timeStamp = today.toISOString().split("T")[0];
+
+  try {
+    if (status) {
+      condition = "in";
+    } else {
+      condition = "out";
+    }
+    // 构造 JSON 字符串
+    let jsonString = `{"name": "${name}", "idCard": "${idCard}", "status": "${condition}", "timeStamp": "${timeStamp}"}`;
+
+    // 构造 SQL 查询字符串
+    let queryString = `INSERT INTO facedev.key_personnel FORMAT JSONEachRow ${jsonString}`;
+    // 将查询字符串传递给 ClickHouse
+    let params = {
+      query: queryString
+    };
+    await clickhouseDb.exec(params);
+    ctx.body = util.success("添加成功");
+  } catch (error) {
+    // 提供错误的摘要信息
+    ctx.body = util.fail("Failed to delete record: " + error.message);
+  }
+});
+
+/**
  * 小区删除重点人员
  */
 router.post("/deleteRecord", async (ctx) => {
